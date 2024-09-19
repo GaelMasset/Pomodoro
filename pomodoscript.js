b5b1d926684107aa08b89d738fbc3748a6c44adb
gael <gael.masset35@gmail.com>
2024-09-19 22:53:15 +0200

    Bouton Demarrer fonctionnel, décompte aussi, début css

--- pomodoscript.js
+++ pomodoscript.js
@@ -1,25 +1,46 @@
-let decompte = 1500;
+let tempsEtudeChoisi = 60;
+
+let decompte = tempsEtudeChoisi;
 let actif = false;
+let auTravail = false;
+
 function startPomodoro(){
     if(!actif){
         actif = true;
+        auTravail = true;
+        var image = document.getElementById("soleil");
+        image.classList.add("rotate");
     }else{
         actif = false;
+        var image = document.getElementById("soleil");
+        image.classList.remove("rotate");
+        auTravail=false;
     }
+    updateCompteur();
 }
 
 let texte = decompte / 60 + " : " + decompte%60;
 
-setInterval(function(){
+setInterval(updateCompteur, 1000);
+
+function updateCompteur(){
     if(actif){
         decompte --;
         texte = ((decompte / 60).toFixed(0))-1 + " : " + decompte%60;
         document.getElementById("compteur").innerHTML = texte;
+        if(auTravail){
+            document.getElementById("texteOrdre").innerHTML = "Au travail !!!"
+        } else {
+            document.getElementById("texteOrdre").innerHTML = "Tu peux te reposer !"
+        }
     }
     else {
-        decompte = 1500;
-        texte = ((decompte / 60).toFixed(0)) + " : " + decompte%60;
+        decompte = tempsEtudeChoisi;
+        texte = "25 : 00";
         document.getElementById("compteur").innerHTML = texte;
+        document.getElementById("texteOrdre").innerHTML = "Appuies sur le bouton pour commencer"
     }
-}, 1000);
+
+    
+}
 
